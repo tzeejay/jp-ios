295a4af427f59c4cbb9ab6de355758558769ca57
Constantin Jacob <cj@tzeejay.com>
2017-12-17 21:22:58 +0100

    Initial Commit

---
+++ JP-Zeiten/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JP-Zeiten
+//
+//  Created by Constantin Jacob on 17.12.17.
+//  Copyright © 2017 Constantin Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JP-Zeiten/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JP-Zeiten
+//
+//  Created by Constantin Jacob on 17.12.17.
+//  Copyright © 2017 Constantin Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
